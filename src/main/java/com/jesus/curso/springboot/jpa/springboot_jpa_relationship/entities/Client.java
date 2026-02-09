e1015f3976492f4342e74f9815c53d381eb6193a
Jesusmslformacion <chechuelcrack@gmail.com>
2026-02-09 21:38:58 +0100

    Agregar archivos de configuración y estructura inicial del proyecto Spring Boot con JPA

---
+++ src/main/java/com/jesus/curso/springboot/jpa/springboot_jpa_relationship/entities/Client.java
@@ -0,0 +1,11 @@
+package com.jesus.curso.springboot.jpa.springboot_jpa_relationship.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="clients")
+public class Client {
+    
+
+}
---
+++ src/main/java/com/jesus/curso/springboot/jpa/springboot_jpa_relationship/entities/Invoice.java
@@ -0,0 +1,10 @@
+package com.jesus.curso.springboot.jpa.springboot_jpa_relationship.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="invoices")
+public class Invoice {
+
+}
